12b8f693e0114fcc7b331c83ec2f23c2e9108ead
Jason Truong <nghiatruong@usf.edu>
2021-11-06 17:22:36 -0400

    adjust

--- Form1.cs
+++ Form1.cs
@@ -36,8 +36,6 @@ namespace Project2
 
         //also got a deck of card one though lol.
         aDeckOfCards deck;
-        Hand playerHand;
-        Hand computerHand;
         int computerSum;
         int playerSum;
         aShoe dealingShoe;
@@ -57,8 +55,7 @@ namespace Project2
             playerPictureboxList = new ImageList();
             //dealingShoe = new aShoe();
 
-            playerHand = new Hand();
-            computerHand = new Hand();
+            
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -96,7 +93,7 @@ namespace Project2
 
         }
 
-
+        /*
         //need picture box though
         private void updatePicturebox(ref PictureBox pBox, ref Hand hand)
         {
@@ -133,7 +130,7 @@ namespace Project2
 
             //done putting in
             pBox.Image = combinedBitmap;
-        }
+        }*/
 
         private void computerPicturebox_Click(object sender, EventArgs e)
         {
@@ -152,16 +149,7 @@ namespace Project2
 
         private void resetButton_Click(object sender, EventArgs e)
         {
-            //reset pictureboxes
-            computerPicturebox.Image = null;
-            playerPicturebox.Image = null;
-
-            //delete player and delear previous game cards
-            playerHand.Cards.Clear();
-            computerHand.Cards.Clear();
-
-            totalTextBox.Text = "100";
-            betTextBox.Text = "";
+            
         }
 
         /// <summary>
@@ -172,121 +160,12 @@ namespace Project2
 
         private void hitButton_Click(object sender, EventArgs e)
         {
-            /*
-            aCard a = new aCard();
-
-            aCard drawn = decks[0].cards[decks[0].cards.Count - 1];
-
-            //remvove from deck
-            decks[0].cards.Remove(drawn);
-
-            //add to hands
-            //hand.Cards.Add(drawn);
-            */
-
-
-            //shuffle is an aShoe obj
-            //IDrawCard interfaceObj = dealingShoe;
-
-
-            //draw a card and remove from deck.
-            aCard aSingleCard = dealingShoe.Draw();
-
-            //add to hands 
-            playerHand.Cards.Add(aSingleCard);
-            playerHand.AddValue(aSingleCard, ref playerSum);
-            playerValueLabel.Text = playerSum.ToString();
-
-            if (playerSum > 21)
-            {
-                winOrLoseLabel.Text = lose;
-            }
-
-            //PUT PLAYER CARD INTO THE PICTUREBOX
-            //prepare and add playerHand cards to picture box
-            List<Bitmap> images = new List<Bitmap>();
-            int totalWidth = 73 * (int)playerHand.Cards.Count();
-            int height = 97;
-
-            //put in the images list.
-            for (int j = 0; j < playerHand.Cards.Count(); j++)
-            {
-                images.Add(playerHand.Cards[j].GetBitmapImage());
-            }
-
-            //create an image(bitmap)
-            Bitmap combinedBitmap = new Bitmap(totalWidth, height);
-
-            //combine the images from the bit map list - with the graphic 
-            Graphics combinedGraphics = Graphics.FromImage(combinedBitmap);
-
-            int drawingPosition = 0;
-
-            //put into combinedGraphic (or a picture)
-            foreach (Bitmap bitmap in images)
-            {
-
-                //draw image into the graphic, cool. 
-                combinedGraphics.DrawImage(bitmap, new Point(drawingPosition, 0));
-                drawingPosition += bitmap.Width;
-            }
-
-            //done putting in
-            playerPicturebox.Image = combinedBitmap;
+            
         }
 
         private void standButton_Click(object sender, EventArgs e)
         {
-            if (gameModeComboBox.SelectedItem.ToString() == "H17")
-            {
-                //dealer hits until its == 21 if > then lose:
-                while (computerSum != 21)
-                {
-                    //break the loops
-                    if (computerSum > 21)
-                    {
-                        dealerValueLabel.Text = computerSum.ToString();
-                        winOrLoseLabel.Text = win;
-
-                        //is this break of the loop though
-                        break;
-                    }
-
-                    aCard aSingleCard;
-                    aSingleCard = dealingShoe.Draw();
-                    computerHand.Cards.Add(aSingleCard);
-                    computerHand.AddValue(aSingleCard, ref computerSum);
-                }
-
-                dealerValueLabel.Text = computerSum.ToString();
-                updatePicturebox(ref computerPicturebox, ref computerHand);
-
-            }
-            else
-            {
-                while (computerSum != 21)
-                {
-                    //break the loops
-                    if (computerSum > 21)
-                    {
-                        dealerValueLabel.Text = computerSum.ToString();
-                        winOrLoseLabel.Text = win;
-
-                        //is this break of the loop though
-                        break;
-                    }
-
-                    aCard aSingleCard;
-                    aSingleCard = dealingShoe.Draw();
-                    computerHand.Cards.Add(aSingleCard);
-                    computerHand.AddValue(aSingleCard, ref computerSum);
-                }
-
-                dealerValueLabel.Text = computerSum.ToString();
-
-                //then this update but missing why 2 and 7 got to that number though.
-                updatePicturebox(ref computerPicturebox, ref computerHand);
-            }
+           
         }
 
         private void dealerCards_Click(object sender, EventArgs e)
--- aShoe.cs
+++ aShoe.cs
@@ -1,15 +1,50 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace Project2
 {
-    public class aShoe : IDrawCard
+    /// <summary>
+    /// creating a shuffling machine or a shoe containing potentially up to 8 decks of cards
+    /// </summary>
+    public class aShoe //: IDrawCard
     {
         /// <summary>
-        /// 
+        /// object shoe as read only at the runtime
         /// </summary>
-        private List<aDeckOfCards> shoe = new List<aDeckOfCards>();
+        private readonly List<aDeckOfCards> shoe = new List<aDeckOfCards>();
 
+        /// <summary>
+        /// number of deck
+        /// </summary>
+        int numberOfDecks;
+
+        /// <summary>
+        /// Contructor with the default number of decks is 1 if no argument passed
+        /// </summary>
+        /// <param name="numberOfDecks"></param>
+        public aShoe(int numberOfDecks = 1)
+        {
+            this.numberOfDecks = numberOfDecks;
+            for (int i = 0; i < numberOfDecks; ++i)
+            {
+                shoe.Add(new aDeckOfCards());
+            }
+
+        }
+
+        /*
+        public aCard Draw()
+        {
+            Random rand = new Random();
+            int value = rand.Next(0, deck.Count);
+            aCard drawCard = deck[value];
+
+            deck.RemoveAt(value);
+
+            return drawCard;
+
+        }*/
     }
  
 
